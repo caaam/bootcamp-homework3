56c262ac003cb2dcb20740098db1e9b973702bf6
cam <cam@camherbert.com>
2021-08-21 17:59:26 -0400

    Added logic to check length and character set, added password generation and output. Removed debug logging

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var specialCharset = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var numericCharset = "0123456789";
+let lowercaseCharset = "abcdefghijklmnopqrstuvwxyz";
+let uppercaseCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let specialCharset = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+let numericCharset = "0123456789";
 
 function generatePassword() {
-  console.log("breakpoint 1");
   let passwordLength = prompt("How many characters would you like your password to contain?");
   var numericPasswordLength = parseInt(passwordLength);
   if (passwordLength == null || passwordLength == "" || isNaN(numericPasswordLength)) {
@@ -16,10 +15,37 @@ function generatePassword() {
   } else if (numericPasswordLength < 8 || numericPasswordLength > 128) {
     alert("Password must be between 8 and 128 characters");
     return;
-  } else {
-    alert("Password will be " + numericPasswordLength + " characters long");
   }
-  console.log("breakpoint 2");
+
+  let availableCharset = "";
+
+  if (confirm("Click OK to confirm using lowercase characters")) {
+    availableCharset += lowercaseCharset;
+  }
+  if (confirm("Click OK to confirm using uppercase characters")) {
+    availableCharset += uppercaseCharset;
+  }
+  if (confirm("Click OK to confirm using numeric characters")) {
+    availableCharset += numericCharset;
+  }
+  if (confirm("Click OK to confirm using special characters")) {
+    availableCharset += specialCharset;
+  }
+
+  if (availableCharset == "") {
+    alert("You must include some character type for the password");
+  }
+
+  var password = "";
+
+  let usableCharacters = availableCharset.split("");
+
+  for (let i = 0; i <= numericPasswordLength; i++) {
+    password += usableCharacters[getRandomInt(availableCharset.length)];
+  }
+
+  return password;
+
 }
 
 // Write password to the #password input
@@ -29,6 +55,10 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function getRandomInt(maxValue) {
+  return Math.floor(Math.random() * maxValue);
 }
 
 // Add event listener to generate button
